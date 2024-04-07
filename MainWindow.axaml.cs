e2361c4131767281ace6d7cbc2be0d740be3ce3f
Дмитрий Серов <dima.bessarab@list.ru>
2024-04-08 01:51:53 +0300

    iui

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,21 +1,38 @@
 using Avalonia.Controls;
 using System.Linq;
+using System.Net.Mime;
+using Avalonia.Interactivity;
 
 namespace reg
 {
     public partial class MainWindow : Window
     {
+        private Number _number = new Number();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = _number;
         }
-
-
+        
+        private class Number
+        {
+            public string phone_number { get; set; }
+        }
 
         private void TextBox_KeyUp(object? sender, Avalonia.Input.KeyEventArgs e)
         {
             if (Phone.Text.Length >= 7)
                 IN.IsVisible = true;
+        }
+
+        private void ToSecWindow(object? sender, RoutedEventArgs e)
+        {
+            var _win1 = new Window1()
+            {
+                DataContext = _number
+            };
+            _win1.Show();
         }
     }
 }
---
+++ Window1.axaml.cs
@@ -0,0 +1,17 @@
+using System.Runtime.InteropServices.JavaScript;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace reg;
+
+public partial class Window1 : Window
+{
+    public Window1()
+    {
+        
+        InitializeComponent();
+    }
+
+    
+}
